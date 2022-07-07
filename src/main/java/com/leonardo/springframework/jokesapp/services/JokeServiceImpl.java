60149feb81e222f61153445827c663d7f702ed8d
leogutschow <leogusalles@hotmail.com>
2022-07-07 15:41:36 -0300

    Implementing Controllers and the Index HTML

--- src/main/java/com/leonardo/springframework/jokesapp/services/JokeServiceImpl.java
+++ src/main/java/com/leonardo/springframework/jokesapp/services/JokeServiceImpl.java
@@ -1,14 +1,19 @@
 package com.leonardo.springframework.jokesapp.services;
+
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    private final ChuckNorrisQuotes jokes = new ChuckNorrisQuotes();
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
 
     @Override
     public String getJoke() {
-        return jokes.getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
